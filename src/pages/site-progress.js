eb0321b32df1f467e5090671ca267302cc4aeaef
michael-small <small203@morris.umn.edu>
2021-03-24 14:11:49 -0500

    Delete .tsx page, convert page-2 to site WIP info

--- src/pages/site-progress.js
+++ src/pages/site-progress.js
@@ -4,13 +4,13 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const SiteProgress = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <SEO title="Site Progress" />
+    <h1>Site Progress</h1>
+    <p>Where Michael will give updates on how the site is coming along</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
-export default SecondPage
+export default SiteProgress
